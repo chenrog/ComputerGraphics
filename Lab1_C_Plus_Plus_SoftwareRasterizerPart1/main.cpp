5ae9c020f39655604f16354be9bef95a6f1820e8
RogerChen98 <chen.rog@husky.neu.edu>
2020-01-21 17:14:47 -0500

    splotchy triangles

--- Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
+++ Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
@@ -14,15 +14,13 @@
  *  @bug No known bugs.
  */
 
-// Load our libraries
-#include <iostream>
-
 // Some define values
 #define WINDOW_HEIGHT 320
 #define WINDOW_WIDTH 320
 
 // C++ Standard Libraries
 #include <iostream>
+#include <unordered_map>
 
 // User libraries
 #include "Color.h"
@@ -32,12 +30,36 @@
 
 // Create a canvas to draw on.
 TGA canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+std::unordered_map<int, int*> triangle_points;
+
+void record_point(int x, int y) {
+  if (triangle_points.find(y) == triangle_points.end()) {
+    int* arr = new int[2];
+    arr[0] = x;
+    arr[1] = x;
+    triangle_points[y] = arr;
+  }
+  // If key found then iterator to that key is returned
+  else {
+    int* existing_x = triangle_points.at(y);
+
+    int min = *existing_x;
+    int max = *(existing_x + 1);
+
+    if (x < min) {
+      *existing_x = x;
+    }
+    if (x > max) {
+      *(existing_x + 1) = x;
+    }
+  }
+}
 
 // Implementation of Bresenham's Line Algorithm
 // The input to this algorithm is two points and a color
 // This algorithm will then modify a canvas (i.e. image)
 // filling in the appropriate colors.
-void drawLine(Vec2 v0, Vec2 v1, TGA& image, ColorRGB c) {
+void drawLine(Vec2 v0, Vec2 v1, TGA& image, ColorRGB c, bool record = false) {
   bool steep = false;
   if (std::abs(v0.x - v1.x) < std::abs(v0.y - v1.y)) {
     // If the line is steep we want to transpose the image.
@@ -54,8 +76,14 @@ void drawLine(Vec2 v0, Vec2 v1, TGA& image, ColorRGB c) {
     int y = v0.y * (1.0f - t) + v1.y * t;
     if (steep) {
       canvas.setPixelColor(y, x, c);
+      if (record) {
+        record_point(y, x);
+      }
     } else {
       canvas.setPixelColor(x, y, c);
+      if (record) {
+        record_point(x, y);
+      }
     }
   }
 }
@@ -68,15 +96,36 @@ void triangle(Vec2 v0, Vec2 v1, Vec2 v2, TGA& image, ColorRGB c) {
     drawLine(v2, v0, image, c);
   }
   // TODO: Draw a filled triangle
+  if (glFillMode == FILL) {
+    drawLine(v0, v1, image, c, true);
+    drawLine(v1, v2, image, c, true);
+    drawLine(v2, v0, image, c, true);
+
+    std::unordered_map<int, int*>::iterator itr;
+    int counter = 0;
+    for (itr = triangle_points.begin(); itr != triangle_points.end(); itr++) {
+        int y = itr->first;
+        int* xs = itr->second;
+        int x1 = *xs;
+        int x2 = *(xs + 1);
+
+        Vec2 v4 = Vec2(x1, y);
+        Vec2 v5 = Vec2(x2, y);
+        drawLine(v4, v5, image, c);
+
+        delete[] itr->second;
+     }
+
+     triangle_points.clear();
+  }
 }
 
 // Main
 int main() {
   // A sample of color(s) to play with
-  ColorRGB red;
-  red.r = 255;
-  red.g = 0;
-  red.b = 0;
+  ColorRGB red = {255, 0, 0};
+  ColorRGB green = {0, 255, 0};
+  ColorRGB blue = {0, 0, 255};
 
   // Points for our Line
   Vec2 line[2] = {Vec2(0, 0), Vec2(100, 100)};
@@ -85,13 +134,17 @@ int main() {
   glPolygonMode(FILL);
 
   // Draw a line
-  drawLine(line[0], line[1], canvas, red);
+  // drawLine(line[0], line[1], canvas, red);
 
   // Data for our triangle
-  Vec2 tri[3] = {Vec2(160, 60), Vec2(150, 10), Vec2(75, 190)};
+  Vec2 tri[3] = {Vec2(30, 30), Vec2(100, 90), Vec2(50, 75)};
+  Vec2 tri2[3] = {Vec2(250, 150), Vec2(200, 60), Vec2(150, 150)};
+  Vec2 tri3[3] = {Vec2(75, 300), Vec2(75, 250), Vec2(50, 250)};
 
   // Draw a triangle
   triangle(tri[0], tri[1], tri[2], canvas, red);
+  triangle(tri2[0], tri2[1], tri2[2], canvas, green);
+  triangle(tri3[0], tri3[1], tri3[2], canvas, blue);
 
   // Output the final image
   canvas.outputTGAImage("graphics_lab2.ppm");
