6d2bd18c14ac2beb87a087346dfb97ded99ea498
RogerChen98 <chen.rog@husky.neu.edu>
2020-02-18 02:09:53 -0500

    object parsing online hopefully

--- Assignment3_ModelParser/ObjParser.cpp
+++ Assignment3_ModelParser/ObjParser.cpp
@@ -2,8 +2,42 @@
 
 //////////////////////////////////////////////////////////////////////
 // Publics
-ObjParser::ObjParser() { std::cout << "yes" << std::endl; }
+ObjParser::ObjParser() {}
 
 ObjParser::~ObjParser() {}
 
-void ObjParser::help() { std::cout << "help" << std::endl; }
+void ObjParser::parse(std::string fileName) {
+  std::ifstream file;
+  file.open("./objects/" + fileName);
+
+  std::string line;
+  while (getline(file, line)) {
+    QString qline = QString::fromStdString(line);
+    QStringList data = qline.split(' ', QString::SkipEmptyParts);
+
+    // vertex normal
+    if (line[0] == 'v' && line[1] == 'n') {
+      for (int i = 0; i < data.size(); i++) {
+        vertex_normals.push_back(data[i].toFloat());
+      }
+    }
+    // vertex
+    else if (line[0] == 'v') {
+      for (int i = 0; i < data.size(); i++) {
+        vertices.push_back(data[i].toFloat());
+      }
+    }
+    // face
+    else if (line[0] == 'f') {
+      for (int i = 0; i < data.size(); i++) {
+        indices.push_back(data[i].toUInt());
+      }
+    }
+  }
+}
+
+QVector<float> ObjParser::getVertices() { return vertices; }
+
+QVector<float> ObjParser::getVertexNormals() { return vertex_normals; }
+
+QVector<uint> ObjParser::getIndices() { return indices; }
--- Assignment3_ModelParser/ObjParser.h
+++ Assignment3_ModelParser/ObjParser.h
@@ -1,14 +1,25 @@
 #pragma once
 
+#include <QString>
+#include <QStringList>
+#include <QVector>
+#include <fstream>
 #include <iostream>
 #include <string>
 
 class ObjParser {
  private:
  protected:
+  QVector<float> vertices;
+  QVector<float> vertex_normals;
+  QVector<uint> indices;
+
  public:
   ObjParser();
   virtual ~ObjParser();
 
-  void help();
+  void parse(std::string fileName);
+  QVector<float> getVertices();
+  QVector<float> getVertexNormals();
+  QVector<uint> getIndices();
 };
