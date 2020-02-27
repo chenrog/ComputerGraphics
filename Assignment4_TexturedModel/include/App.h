a6af0b8fac5457676103f9215e47a965bdccb326
RogerChen98 <chen.rog@husky.neu.edu>
2020-02-27 01:32:26 -0500

    made a better folder structure for cmake

---
+++ Assignment4_TexturedModel/include/App.h
@@ -0,0 +1,22 @@
+/**
+ * The primary application code.
+ */
+
+#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
+
+class App : public QMainWindow {
+  Q_OBJECT
+
+ public:
+  App(QWidget* parent = 0);
+  virtual ~App();
+
+ signals:
+
+ public slots:
+
+ private:
+  void buildGui();
+};
--- Assignment4_TexturedModel/main.cpp
+++ Assignment4_TexturedModel/main.cpp
@@ -6,8 +6,9 @@
 #include <QtGui>
 #include <QtOpenGL>
 
-// TODO:  you have to include whatever application-specific code there is here.  This should be
-// a subclass of QMainWindow!
+// TODO:  you have to include whatever application-specific code there is here.
+// This should be a subclass of QMainWindow!
+#include "App.h"
 
 int main(int argc, char** argv) {
   QApplication a(argc, argv);
@@ -17,12 +18,13 @@ int main(int argc, char** argv) {
   QSurfaceFormat fmt;
   fmt.setDepthBufferSize(24);
   fmt.setStencilBufferSize(8);
-  fmt.setVersion(3,3);
+  fmt.setVersion(3, 3);
   fmt.setProfile(QSurfaceFormat::CoreProfile);
   QSurfaceFormat::setDefaultFormat(fmt);
 
-  // TODO:  Replace the following 3 lines with whatever you need to create, show, and execute your application
-  //Application app;
-  //app.show();
-  //return QApplication::exec();
+  // TODO:  Replace the following 3 lines with whatever you need to create,
+  // show, and execute your application
+  App app;
+  app.show();
+  return QApplication::exec();
 }
