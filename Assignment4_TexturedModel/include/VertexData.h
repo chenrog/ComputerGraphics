8e1f3cde95910d3009d781bbff9125b88e2cb266
RogerChen98 <chen.rog@husky.neu.edu>
2020-03-08 01:18:00 -0500

    vertex data was made more robust

--- Assignment4_TexturedModel/include/VertexData.h
+++ Assignment4_TexturedModel/include/VertexData.h
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <QtGui>
+
 struct VertexData {
   float x, y, z;
   float s, t;
 
   VertexData(float _x, float _y, float _z, float _s, float _t)
       : x(_x), y(_y), z(_z), s(_s), t(_t) {}
+
+  VertexData(QVector3D position, QVector2D texture)
+      : x(position.x()),
+        y(position.y()),
+        z(position.z()),
+        s(texture.x()),
+        t(texture.y()) {}
 
   // Tests if two VertexData are equal
   bool operator==(const VertexData &other) {
@@ -15,4 +24,8 @@ struct VertexData {
            (s == other.s) &&
            (t == other.t);
   }
+
+  QVector3D position() { return QVector3D(x, y, z); }
+
+  QVector3D texture() { return QVector2D(s, t); }
 };
