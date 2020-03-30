058978400f9917f4ce49342836744afa2ba6107b
RogerChen98 <chen.rog@husky.neu.edu>
2020-03-30 19:27:15 -0400

    camera work done

--- Lab8_CameraAndIllumination/BasicWidget.cpp
+++ Lab8_CameraAndIllumination/BasicWidget.cpp
@@ -4,35 +4,35 @@
 
 //////////////////////////////////////////////////////////////////////
 // Publics
-BasicWidget::BasicWidget(QWidget* parent) : QOpenGLWidget(parent), logger_(this)
-{
+BasicWidget::BasicWidget(QWidget* parent)
+    : QOpenGLWidget(parent), logger_(this) {
   setFocusPolicy(Qt::StrongFocus);
   camera_.setPosition(QVector3D(0.5, 0.5, -2.0));
   camera_.setLookAt(QVector3D(0.5, 0.5, 0.0));
   world_.setToIdentity();
 }
 
-BasicWidget::~BasicWidget()
-{
-    for (auto renderable : renderables_) {
-        delete renderable;
-    }
-    renderables_.clear();
+BasicWidget::~BasicWidget() {
+  for (auto renderable : renderables_) {
+    delete renderable;
+  }
+  renderables_.clear();
 }
 
 //////////////////////////////////////////////////////////////////////
 // Privates
 ///////////////////////////////////////////////////////////////////////
 // Protected
-void BasicWidget::keyReleaseEvent(QKeyEvent* keyEvent)
-{
+void BasicWidget::keyReleaseEvent(QKeyEvent* keyEvent) {
   // Handle key events here.
   if (keyEvent->key() == Qt::Key_Left) {
     qDebug() << "Left Arrow Pressed";
-    update();  // We call update after we handle a key press to trigger a redraw when we are ready
+    update();  // We call update after we handle a key press to trigger a redraw
+               // when we are ready
   } else if (keyEvent->key() == Qt::Key_Right) {
     qDebug() << "Right Arrow Pressed";
-    update();  // We call update after we handle a key press to trigger a redraw when we are ready
+    update();  // We call update after we handle a key press to trigger a redraw
+               // when we are ready
   } else if (keyEvent->key() == Qt::Key_R) {
     camera_.setPosition(QVector3D(0.5, 0.5, -2.0));
     camera_.setLookAt(QVector3D(0.5, 0.5, 0.0));
@@ -42,8 +42,7 @@ void BasicWidget::keyReleaseEvent(QKeyEvent* keyEvent)
   }
 }
 
-void BasicWidget::mousePressEvent(QMouseEvent* mouseEvent)
-{
+void BasicWidget::mousePressEvent(QMouseEvent* mouseEvent) {
   if (mouseEvent->button() == Qt::LeftButton) {
     mouseAction_ = Rotate;
   } else if (mouseEvent->button() == Qt::RightButton) {
@@ -52,8 +51,7 @@ void BasicWidget::mousePressEvent(QMouseEvent* mouseEvent)
   lastMouseLoc_ = mouseEvent->pos();
 }
 
-void BasicWidget::mouseMoveEvent(QMouseEvent* mouseEvent)
-{
+void BasicWidget::mouseMoveEvent(QMouseEvent* mouseEvent) {
   if (mouseAction_ == NoAction) {
     return;
   }
@@ -61,27 +59,30 @@ void BasicWidget::mouseMoveEvent(QMouseEvent* mouseEvent)
   lastMouseLoc_ = mouseEvent->pos();
   if (mouseAction_ == Rotate) {
     // TODO:  Implement rotating the camera
+    camera_.translateLookAt(
+        QVector3D::crossProduct(camera_.lookAt() - camera_.position(),
+                                camera_.upVector()) *
+        0.001f * delta.x());
+
   } else if (mouseAction_ == Zoom) {
     // TODO:  Implement zoom by moving the camera
-    // Zooming is moving along the gaze direction by some amount.
-  } 
+    camera_.translateCamera(camera_.gazeVector() * 0.03f * -delta.y());
+  }
   update();
 }
 
-void BasicWidget::mouseReleaseEvent(QMouseEvent* mouseEvent)
-{
-    mouseAction_ = NoAction;
+void BasicWidget::mouseReleaseEvent(QMouseEvent* mouseEvent) {
+  mouseAction_ = NoAction;
 }
 
-void BasicWidget::initializeGL()
-{
+void BasicWidget::initializeGL() {
   makeCurrent();
   initializeOpenGLFunctions();
 
   qDebug() << QDir::currentPath();
   // TODO:  You may have to change these paths.
-  QString brickTex = "../../brick.ppm";
-  QString grassTex = "../../grass.ppm";
+  QString brickTex = "./brick.ppm";
+  QString grassTex = "./grass.ppm";
 
   UnitQuad* backWall = new UnitQuad();
   backWall->init(brickTex);
@@ -122,27 +123,25 @@ void BasicWidget::initializeGL()
   frameTimer_.start();
 }
 
-void BasicWidget::resizeGL(int w, int h)
-{
-    if (!logger_.isLogging()) {
-        logger_.initialize();
-        // Setup the logger for real-time messaging
-        connect(&logger_, &QOpenGLDebugLogger::messageLogged, [=]() {
-            const QList<QOpenGLDebugMessage> messages = logger_.loggedMessages();
-            for (auto msg : messages) {
-                qDebug() << msg;
-            }
-            });
-        logger_.startLogging();
-    }
+void BasicWidget::resizeGL(int w, int h) {
+  if (!logger_.isLogging()) {
+    logger_.initialize();
+    // Setup the logger for real-time messaging
+    connect(&logger_, &QOpenGLDebugLogger::messageLogged, [=]() {
+      const QList<QOpenGLDebugMessage> messages = logger_.loggedMessages();
+      for (auto msg : messages) {
+        qDebug() << msg;
+      }
+    });
+    logger_.startLogging();
+  }
   glViewport(0, 0, w, h);
 
   camera_.setPerspective(70.f, (float)w / (float)h, 0.001, 1000.0);
   glViewport(0, 0, w, h);
 }
 
-void BasicWidget::paintGL()
-{
+void BasicWidget::paintGL() {
   qint64 msSinceRestart = frameTimer_.restart();
   glDisable(GL_DEPTH_TEST);
   glDisable(GL_CULL_FACE);
@@ -153,9 +152,11 @@ void BasicWidget::paintGL()
   glEnable(GL_DEPTH_TEST);
 
   for (auto renderable : renderables_) {
-      renderable->update(msSinceRestart);
-      // TODO:  Understand that the camera is now governing the view and projection matrices
-      renderable->draw(world_, camera_.getViewMatrix(), camera_.getProjectionMatrix());
+    renderable->update(msSinceRestart);
+    // TODO:  Understand that the camera is now governing the view and
+    // projection matrices
+    renderable->draw(world_, camera_.getViewMatrix(),
+                     camera_.getProjectionMatrix());
   }
   update();
 }
--- Lab8_CameraAndIllumination/Camera.h
+++ Lab8_CameraAndIllumination/Camera.h
@@ -3,38 +3,36 @@
 #include <QtGui/QMatrix4x4>
 #include <QtGui/QVector3D>
 
-class Camera
-{
-protected:
-	QVector3D position_;
-	QVector3D lookAt_;
-	QVector3D up_;
-	QMatrix4x4 projection_;
+class Camera {
+ protected:
+  QVector3D position_;
+  QVector3D lookAt_;
+  QVector3D up_;
+  QMatrix4x4 projection_;
 
-public:
-	Camera();
-	virtual ~Camera();
+ public:
+  Camera();
+  virtual ~Camera();
 
-	// Manipulate our Camera
-	void setPerspective(float fov, float aspect, float near, float far);
+  // Manipulate our Camera
+  void setPerspective(float fov, float aspect, float near, float far);
 
-	// Move our position.
-	void setPosition(const QVector3D& position);
-	QVector3D position() const;
-	void translateCamera(const QVector3D& delta);
+  // Move our position.
+  void setPosition(const QVector3D& position);
+  QVector3D position() const;
+  void translateCamera(const QVector3D& delta);
 
-	// Move our gaze
-	void setGazeVector(const QVector3D& gaze);
-	QVector3D gazeVector() const;
-	QVector3D upVector() const;
-	void setLookAt(const QVector3D& lookAt);
-	void translateLookAt(const QVector3D& delta);
-	QVector3D lookAt() const;
+  // Move our gaze
+  void setGazeVector(const QVector3D& gaze);
+  QVector3D gazeVector() const;
+  QVector3D upVector() const;
+  void setLookAt(const QVector3D& lookAt);
+  void translateLookAt(const QVector3D& delta);
+  QVector3D lookAt() const;
 
-	// Get our camera matrix
-	QMatrix4x4 getViewMatrix() const;
-	QMatrix4x4 getProjectionMatrix() const;
+  // Get our camera matrix
+  QMatrix4x4 getViewMatrix() const;
+  QMatrix4x4 getProjectionMatrix() const;
 
-private:
-
+ private:
 };
