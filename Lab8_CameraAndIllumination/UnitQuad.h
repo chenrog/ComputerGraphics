60eaf12a0e18b80c377e5e8cfa2e1bc271e838c8
RogerChen98 <chen.rog@husky.neu.edu>
2020-03-31 17:00:46 -0400

    autoformating

--- Lab8_CameraAndIllumination/UnitQuad.h
+++ Lab8_CameraAndIllumination/UnitQuad.h
@@ -2,19 +2,18 @@
 
 #include "Renderable.h"
 
-class UnitQuad : public Renderable
-{
-protected:
-	QVector3D lightPos_;
-	float sign_;
-public:
-	UnitQuad();
-	virtual ~UnitQuad();
+class UnitQuad : public Renderable {
+ protected:
+  QVector3D lightPos_;
+  float sign_;
 
-	// Our init method is much easier now.  We only need a texture!
-	virtual void init(const QString& textureFile);
-	virtual void update(const qint64 msSinceLastFrame) override;
+ public:
+  UnitQuad();
+  virtual ~UnitQuad();
 
-private:
+  // Our init method is much easier now.  We only need a texture!
+  virtual void init(const QString& textureFile);
+  virtual void update(const qint64 msSinceLastFrame) override;
 
+ private:
 };
